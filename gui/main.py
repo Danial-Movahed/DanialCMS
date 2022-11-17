36c291595a95dd22bcca411922940cee435a06cc
Seyed Danial Movahed <movaheddanial@gmail.com>
2022-11-17 20:06:50 +0330

    Added blog setup wizard and refresh methods

--- gui/main.py
+++ gui/main.py
@@ -3,19 +3,30 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtMultimedia import *
 from PyQt5.QtMultimediaWidgets import *
+import ui_BlogPicker
+import ui_CreateBlogWizard
 
-from ui_First import Ui_MainWindow
-
-
-class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+class CreateBlogWizard(QMainWindow, ui_CreateBlogWizard.Ui_MainWindow):
+    def __init__(self):
+        super(CreateBlogWizard, self).__init__()
         self.setupUi(self)
         self.show()
+class BlogPicker(QMainWindow, ui_BlogPicker.Ui_MainWindow):
+    def __init__(self):
+        super(BlogPicker, self).__init__()
+        self.setupUi(self)
+        self.show()
+        self.NewBlogbtn.clicked.connect(lambda: self.startWizard())
+        self.Refreshbtn.clicked.connect(lambda: self.refresh())
+    def startWizard(self):
+       self.wzd = CreateBlogWizard()
+       self.wzd.closeEvent = self.refresh
+    def refresh(self,e=None):
+        print("aa")
 
 
 if __name__ == '__main__':
     app = QApplication([])
     app.setApplicationName("DanialCMS")
-    wnd = MainWindow()
+    wnd = BlogPicker()
     app.exec()
